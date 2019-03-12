3f679702525a001e85f3c0b4417a0783cdb88788
Rajveer <kaurrajveer1201@gmail.com>
2019-03-13 09:41:48 +1300

    ssecond commit

---
+++ 2-raj/2-raj/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_raj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, enter your lucky number here");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter another number");
+            int num2 = int.Parse(Console.ReadLine());
+            double result1 = num1 + num2;
+            double result2 = num1 * num2;
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+        }
+    }
+}
